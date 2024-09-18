25e37911a14427d427da3fd1881e2f7b0d38e158
dudaslevente <dudaslevente@turr.hu>
2024-09-18 09:33:12 +0200

    Frontend

--- Frontend/Assets/js/app.js
+++ Frontend/Assets/js/app.js
@@ -3,6 +3,7 @@ const Author = "13.a SZOFT";
 const Company = "Bajai SZC Türr István Technikum";
 
 const serverUrl = 'http://localhost:3000';
+let loggedUser = null;
 
 let title = document.querySelector('title');
 let header = document.querySelector('header');
@@ -15,4 +16,11 @@ footer.innerHTML = Company + ' | ' + Author + ' | 2024.';
 async function render(view){
     let main = document.querySelector('main');
     main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
+}
+
+if (localStorage.getItem('pekseg')){
+    loggedUser = JSON.parse(localStorage.getItem('pekseg'));
+    render('recipes');
+}else{
+    render('login');
 }
--- Frontend/Controller/userCtrl.js
+++ Frontend/Controller/userCtrl.js
@@ -1,5 +1,31 @@
-function login(){}
+function login(){
+    let user = {
+        email: document.querySelector('#email').value,
+        passwd: document.querySelector('#email').passwd
+    }
 
-function registration(){}
+    axios.post(`${serverUrl}/login`, users).then(res => {
+        loggedUser = res.data;
+        localStorage.setItem('pekseg', JSON.stringify(loggedUser));
+        render('recipes')
+    });
+}
 
-function logout(){}
+function registration(){
+    let newUser = {
+        name: document.querySelector('#name').value,
+        email: document.querySelector('#email').value,
+        passwd: document.querySelector('#passwd').value,
+        confirm: document.querySelector('#confirm').value
+    }
+
+    axios.post(`${serverUrl}/reg`, newUser).then(res => {
+        alert(res.data);
+    });
+}
+
+function logout(){
+    localStorage.removeItem('pekseg');
+    loggedUser = null;
+    render('login');
+}
