745e2a9973e844cc3acb644d330ae312972efc3a
KovagoLevente <kovagolevente@turr.hu>
2024-09-18 11:02:49 +0200

    bejelentkezás szerkesztése kinézet

--- Frontend/Assets/js/app.js
+++ Frontend/Assets/js/app.js
@@ -5,12 +5,12 @@ const Company = "Bajai SZC Türr István Technikum";
 const serverUrl = 'http://localhost:3000';
 let loggedUser = null;
 
-let title = document.querySelector('title');
-let header = document.querySelector('header');
+//let title = document.querySelector('title');
+//let header = document.querySelector('header');
 let footer = document.querySelector('footer');
 
-title.innerHTML = AppTitle;
-header.innerHTML = title.innerHTML;
+//title.innerHTML = AppTitle;
+//header.innerHTML = title.innerHTML;
 footer.innerHTML = Company + ' | ' + Author + ' | 2024.';
 
 async function render(view){
